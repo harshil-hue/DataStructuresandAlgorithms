311df97619a1520caa7bc7e3091fae0dad8b91b6
Harshil <hsllife75@gmail.com>
2026-07-08 02:15:02 +0530

    do while code added

--- src/Loops/doWhileLoop.java
+++ src/Loops/doWhileLoop.java
@@ -1,4 +1,22 @@
 package Loops;
 
 public class doWhileLoop {
+
+    public static void main(String[] args){
+
+        /*
+         do{
+            // execute the code atleast once
+
+         }
+         while(condition)
+
+        */
+
+
+        do{
+            System.out.println("Hello");
+        }
+        while(false);
+    }
 }
